bbd8805a9f349afa61f6e07672d99852faa68e64
JonaPlaz <jplazanet@hotmail.com>
2024-12-22 08:15:25 +0100

    feat: update contract chess template

--- contracts/ChessTemplate.sol
+++ contracts/ChessTemplate.sol
@@ -8,6 +8,8 @@ contract ChessTemplate is ChessControl {
 	address public player2;
 	uint256 public betAmount;
 	bool public gameActive = false;
+	uint16[] private storedMoves;
+	uint8 private currentOutcome;
 
 	event GameStarted(address player1, address player2, uint256 betAmount);
 	event MovePlayed(address player, uint16 move);
@@ -62,33 +64,55 @@ contract ChessTemplate is ChessControl {
 	/// @notice Validates and plays moves in sequence
 	/// @param moves The list of moves played so far
 	function playMove(uint16[] memory moves) external onlyPlayers {
-		require(gameActive, "Game has ended");
+		require(gameActive, "Game is inactive");
+		require(moves.length > 0, "Moves array is empty");
 
 		// Validation de la partie entière via checkGameFromStart
 		(uint8 outcome, , , ) = checkGameFromStart(moves);
+
+		// Mise à jour des moves et de l'outcome
+		storedMoves = moves;
+		currentOutcome = outcome;
 
 		// Envoie un event pour indiquer le dernier coup joué
 		emit MovePlayed(msg.sender, moves[moves.length - 1]);
 
 		// Fin de partie si un résultat a été trouvé
+		// ajouter un ou des évenement pour fin de partie à récupérer dans le front
 		if (outcome != inconclusive_outcome) {
 			_finalizeGame(outcome);
 		}
 	}
 
-	/// @notice Finalizes the game and sends the rewards
+	/// @notice Finalizes the game and distributes the rewards
+	/// @param outcome The result of the game
 	function _finalizeGame(uint16 outcome) internal {
 		gameActive = false;
-		address winner = outcome == white_win_outcome
-			? player1
-			: outcome == black_win_outcome
-				? player2
-				: address(0);
+		address winner;
+
+		if (outcome == white_win_outcome) {
+			winner = player1;
+		} else if (outcome == black_win_outcome) {
+			winner = player2;
+		}
 
 		emit GameEnded(outcome, winner);
 
 		if (winner != address(0)) {
+			// Victoire : le gagnant reçoit tout
 			payable(winner).transfer(betAmount * 2);
+		} else if (outcome == draw_outcome) {
+			// Égalité : chaque joueur récupère sa mise
+			payable(player1).transfer(betAmount);
+			payable(player2).transfer(betAmount);
 		}
+	}
+
+	/// @notice Returns the current game state
+	/// @return moves List of moves played so far
+	/// @return outcome Current outcome of the game
+	function getGameState() external view returns (uint16[] memory moves, uint8 outcome) {
+		moves = storedMoves;
+		outcome = currentOutcome;
 	}
 }
